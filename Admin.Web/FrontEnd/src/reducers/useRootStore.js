fee9f1f782d90b2f24ea0ada858da8fcb44ce058
shahiab <abdul.shahid@ofx.com>
2019-09-20 11:19:01 +1000

    Redux migration.... checked in changes....

--- Admin.Web/FrontEnd/src/reducers/useRootStore.js
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react';
-import { RootContext } from '@shared/App/Provider';
-export default function useRootStore() {
-    return useContext(RootContext);
-}
-//# sourceMappingURL=useRootStore.js.map
