efedc8f3346fff7087b1cdb12e0f97aff16693fe
shahiab <abdul.shahid@ofx.com>
2020-08-12 16:46:58 +1000

    Added Theme

--- admin-client/src/App.tsx
+++ admin-client/src/App.tsx
@@ -1,26 +1,22 @@
+import 'react-perfect-scrollbar/dist/css/styles.css';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useRoutes } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/core';
 
-function App() {
+import GlobalStyles from 'src/components/GlobalStyles';
+import 'src/mixins/chartjs';
+import theme from 'src/theme';
+import routes from 'src/routes';
+
+const App = () => {
+  const routing = useRoutes(routes);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      {routing}
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
---
+++ admin-client/src/components/Logo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from 'src/logo.svg';
+
+const Logo = (props) => {
+  return (
+    <img
+      alt="Logo"
+      src={logo}
+      {...props}
+    />
+  );
+};
+
+export default Logo;
